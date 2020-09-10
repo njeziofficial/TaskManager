7879d99226815a2e2958d8d08914d29fbaa52a20
john.njezi@longbridgetech.com <john.njezi@longbridgetech.com>
2020-09-10 16:35:14 +0100

    Ading Table to the Dashboard page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  Designation: string;
+  Username: string;
+  NoOfTeamMembers: number;
+  TotalCostOfAllProjects: number;
+  PendingTasks: number;
+  UpcomingProjects: number;
+  ProjectCost: number;
+
+  CurrentExpenditure: number;
+  AvailableFunds: number;
 
   ngOnInit(): void {
+    this.Designation = "Team Lead";
+    this.Username = "Adam Warlock";
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpcomingProjects = 2;
+    this.ProjectCost = 25000;
+    this.CurrentExpenditure = 96773;
+    this.AvailableFunds = 53552;
+
   }
 
 }
